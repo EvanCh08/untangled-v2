a490c08a5245da239f6ec1bc39c8fff201dda534
EvanCh08 <evandarren04@gmail.com>
2024-03-25 14:32:57 +0800

    trying

--- server/server.js
+++ server/server.js
@@ -1,36 +1,47 @@
-require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
-const session = require('express-session');
-const {google} = require('googleapis');
-const axios = require('axios');
-const path = require('path')
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const session = require("express-session");
+const { google } = require("googleapis");
+const axios = require("axios");
+const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 3001;
 
-app.use(session({
-  secret: process.env.SESSION_SECRET, // Change this to a random secret string
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: 'auto' }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // Change this to a random secret string
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: "auto" },
+  })
+);
 
-app.use(cors({
+app.use(
+  cors({
     origin: [process.env.CORS_ORIGIN],
-    credentials: true, 
-  }));
+    credentials: true,
+  })
+);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://untangled-v2-frontendv2.onrender.com');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://untangled-v2-main.onrender.com"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const clientId = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-const redirectUri = /*'https://untangled-server.render.com/oauth2callback'*/
-process.env.GOOGLE_REDIRECT_URI;
+const redirectUri =
+  /*'https://untangled-server.render.com/oauth2callback'*/
+  process.env.GOOGLE_REDIRECT_URI;
 
 const oAuth2Client = new google.auth.OAuth2(
   clientId,
@@ -38,64 +49,42 @@ const oAuth2Client = new google.auth.OAuth2(
   redirectUri
 );
 
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
   const url = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: ['https://www.googleapis.com/auth/calendar.readonly', 
-    'https://www.googleapis.com/auth/userinfo.profile'],
+    access_type: "offline",
+    scope: [
+      "https://www.googleapis.com/auth/calendar.readonly",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ],
   });
   res.redirect(url);
 });
 
-app.get('/oauth2callback', async (req, res) => {
+app.get("/oauth2callback", async (req, res) => {
   const { code } = req.query;
-  try {
-    const { tokens } = await oAuth2Client.getToken(code);
-    req.session.tokens = tokens;
-    
-    // Manually save the session before redirecting
-    req.session.save(err => {
-      console.log(req.session.tokens)
-      if (err) {
-        // handle error here, for example by logging it and sending an error response
-        
-        console.error('Session save error:', err);
-        return res.status(500).send('Internal Server Error');
-      }
-      // Only redirect once the session has been saved successfully
-      res.redirect(process.env.REDIRECT_HOME);
-    });
-    
-  } catch (error) {
-    console.error('Error during OAuth2 callback', error);
-    res.status(500).send('Authentication error');
-  }
-  
-//   const { code } = req.query;
-//   const { tokens } = await oAuth2Client.getToken(code);
-//   oAuth2Client.setCredentials(tokens);
-//   // Store the tokens in the session
-//   req.session.tokens = tokens;
-// const redirectHoho = /*'https://untangled-frontend.render.com/home'*/
-// process.env.REDIRECT_HOME
-//   res.redirect(redirectHoho)
+  const { tokens } = await oAuth2Client.getToken(code);
+  oAuth2Client.setCredentials(tokens);
+  // Store the tokens in the session
+  req.session.tokens = tokens;
+  req.session.save(() => {
+    const redirectUrl = process.env.REDIRECT_HOME;
+    res.redirect(redirectUrl);
+  });
 });
 
-app.get('/user-info', async (req, res) => {
-  console.log(req.session.tokens, req.session.tokens.access_token);
-  if (!req.session.tokens || !req.session.tokens.access_token) {
-    return res.status(401).send('User not authenticated');
+app.get("/user-info", async (req, res) => {
+  if (!req.session.tokens) {
+    return res.status(401).send("User not authenticated");
   }
 
   oAuth2Client.setCredentials(req.session.tokens);
 
-  const peopleService = google.people({version: 'v1', auth: oAuth2Client});
+  const peopleService = google.people({ version: "v1", auth: oAuth2Client });
   try {
     const me = await peopleService.people.get({
-      resourceName: 'people/me',
-      personFields: 'names,photos',
+      resourceName: "people/me",
+      personFields: "names,photos",
     });
-
 
     const userInfo = {
       name: me.data.names[0].displayName,
@@ -105,25 +94,28 @@ app.get('/user-info', async (req, res) => {
     res.json(userInfo);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error fetching user info');
+    res.status(500).send("Error fetching user info");
   }
 });
 
-app.get('/fetch-calendar-events', async (req, res) => {
+app.get("/fetch-calendar-events", async (req, res) => {
   if (!req.session.tokens || !req.session.tokens.access_token) {
-    return res.status(401).send('User not authenticated');
+    return res.status(401).send("User not authenticated");
   }
 
   try {
-    const response = await axios.get('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
-      headers: {
-        Authorization: `Bearer ${req.session.tokens.access_token}`,
-      },
-    });
+    const response = await axios.get(
+      "https://www.googleapis.com/calendar/v3/calendars/primary/events",
+      {
+        headers: {
+          Authorization: `Bearer ${req.session.tokens.access_token}`,
+        },
+      }
+    );
     res.json(response.data.items);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error fetching calendar events');
+    res.status(500).send("Error fetching calendar events");
   }
 });
 
